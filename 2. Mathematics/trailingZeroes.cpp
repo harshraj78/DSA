41cc69f1deaaca7466564c88bea4fb02196336f0
Harsh Raj <63678762+harshraj78@users.noreply.github.com>
2023-06-02 00:43:54 +0530

    Create trailingZeroes.cpp

---
+++ 2. Mathematics/trailingZeroes.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// 1. iterative Method:-
+int trailingZeroes(int n)
+{
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+        fact = fact * i;
+    int count = 0;
+    while (fact % 10 == 0)
+    {
+        count++;
+        fact = fact / 10;
+    }
+}
+// 2. effective method:-
+int trailingZeroes(int n)
+{
+    int count = 0;
+    for (int i = 5; i <= n; i = i * 5)
+    {
+        count = count + n / i;
+    }
+    return count;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    cout << "The number of trailing zeroes in " << n << " is " << trailingZeroes(n) << endl;
+    return 0;
+}
