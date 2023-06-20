9994e41c56f895175118fcf2a460c73545e781a5
Harsh Raj <63678762+harshraj78@users.noreply.github.com>
2023-06-20 17:29:15 +0530

    Update 02_bubbleSort.cpp

--- 7. Sorting/02_bubbleSort.cpp
+++ 7. Sorting/02_bubbleSort.cpp
@@ -18,15 +18,26 @@ void bubbleSort(int arr[], int n)
     }
 }
 
-int main()
+// optimized bubble sort:-
+// The above function always runs O(n^2) time even if the array is sorted. It can be optimized by stopping the algorithm if inner loop didn’t cause any swap.
+// Time Complexity: O(n^2) in worst case. O(n) in best case when array is already sorted.
+// Space Complexity: O(1)
+
+void bubbleSortOptimized(int arr[], int n)
 {
-    int arr[] = {5, 4, 3, 2, 1};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    bubbleSort(arr, n);
-
-    for (int x : arr)
-        cout << x << " ";
-
-    return 0;
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) // n-1 passes
+    {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) // n-i-1 comparisons in each pass( n-i-1 because after each pass, the largest element gets placed at the end of the array)
+        {
+            if (arr[j] > arr[j + 1]) // swap if current element is greater than next element
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false) // if no two elements were swapped by inner loop, then break
+            break;
+    }
 }
